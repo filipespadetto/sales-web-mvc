4c9500b8e6bb754408bd4eb8c6f47b228f63c6b4
Filipe <filipespadetto@gmail.com>
2021-01-06 14:14:20 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,20 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Subtitle"] = "Sales Web Application MVC";
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
             return View();
         }
 
